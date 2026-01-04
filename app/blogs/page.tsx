012ec769afd60b64be168730b4aa30c819bdcba3
Bhumesh Mahajan <167601736+Bhumesh01@users.noreply.github.com>
2026-01-04 21:58:30 +0530

    No Waterfalling Model

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -1,19 +1,39 @@
-export default function Blogs(){
+import axios from "axios";
+async function getBlogs(){
+    try{
+        const response = await axios.get("https://jsonplaceholder.typicode.com/posts");
+        if(response.status == 200){
+            return response.data;
+        }
+    }
+    catch(err){
+        return [];
+    }
+}
+type BlogType = {
+    userId: number,
+    id: number,
+    title: string,
+    body: string
+};
+export default async function Blogs(){
+    const blogs:BlogType[] = await getBlogs();
     return(
         <div className="h-screen">
             <div className="p-5 flex justify-between border-b xl:pl-15 xl:pr-15  mb-5 border-b-borderColor">
-                <h1 className="font-extrabold text-3xl "> BLOGS</h1>
+                <h1 className="animate-pulse font-extrabold text-3xl "> BLOGS</h1>
                 <div className="flex gap-5">
                     <Button name="Add Blogs"></Button>
                     <Button  name="Login"></Button>
                 </div>
             </div>
-            <div className="flex flex-col gap-5 justify-center items-center m-5">
-                <Blog name="Bhumi" title="100xDevs Bootcamp" date="January 1, 2026" description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages"></Blog>
-                <Blog name="Bhumi" title="100xDevs Bootcamp" date="January 1, 2026" description="First Ever bootcamp that will make you Job ready"></Blog>
-                <Blog name="Bhumi" title="100xDevs Bootcamp" date="January 1, 2026" description="First Ever bootcamp that will make you Job ready"></Blog>
-                <Blog name="Bhumi" title="100xDevs Bootcamp" date="January 1, 2026" description="First Ever bootcamp that will make you Job ready"></Blog>
-                <Blog name="Bhumi" title="100xDevs Bootcamp" date="January 1, 2026" description="First Ever bootcamp that will make you Job ready"></Blog>
+            <div className="flex flex-col gap-5 justify-center items-center m-5 pb-10">
+                {blogs.map((b, id)=>{
+                    const date = new Date();
+                    return(
+                        <BlogComponent key={id} name="Anonymous" title={b.title} description={b.body} date={String(date.getDate()).padStart(2, "0")+"/"+String(date.getMonth()+1).padStart(2, "0")+"/"+String(date.getFullYear())}></BlogComponent>
+                    )
+                })}
             </div>
         </div>
     )
@@ -25,14 +45,14 @@ function Button(props:{name: string}){
     );
 }
 
-interface BlogType{
+interface BlogInterface{
     name: string,
     title: string,
     description: string,
     date: string,
 }
 
-function Blog(props:BlogType){
+function BlogComponent(props:BlogInterface){
     return(
         <div className="w-full max-w-4xl border p-5 rounded-2xl border-borderColor">
             <div className="flex justify-between text-sm mb-5">
