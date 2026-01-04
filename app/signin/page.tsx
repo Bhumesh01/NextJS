cb7615701d8ae7c1fb106773b4cff0ce315b3694
Bhumesh Mahajan <167601736+Bhumesh01@users.noreply.github.com>
2026-01-04 19:31:13 +0530

    Created SignUp and SignIn Page

---
+++ app/signin/page.tsx
@@ -0,0 +1,28 @@
+export default function SignIn() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="bg-white border border-borderColor w-fit rounded-xl p-10 text-lg">
+        <h1 className="text-5xl text-center font-extrabold">Sign In</h1>
+        <LabelledInput label="Username" type="text" placeholder="Enter your username"></LabelledInput>
+        <LabelledInput label="Password" type="password" placeholder="Enter your password"></LabelledInput>
+        <div className="text-center m-3">
+          <button className="bg-bgButton text-textButton rounded-2xl p-3 font-medium w-full  hover:bg-hoverbutton active:bg-hoverbutton">Sign In</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+interface LabelledInputType{
+    label: string,
+    placeholder: string,
+    type?: string
+}
+
+function LabelledInput(props:LabelledInputType){
+    return(
+        <div className="mt-5 mb-5">
+          <h3 className="text-xl font-medium">{props.label}</h3>
+          <input className="p-2 border border-borderColor rounded-lg w-xs h-10" placeholder={props.placeholder} type={props.type} ></input>
+        </div>
+    )
+}
---
+++ app/signup/page.tsx
@@ -0,0 +1,20 @@
+export default function SignUp() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="border border-borderColor w-fit rounded-xl p-10 text-lg shadow-xl">
+        <h1 className="text-5xl text-center font-extrabold">Sign Up</h1>
+        <div className="mt-5 mb-5">
+          <h3 className="text-xl font-medium">Username</h3>
+          <input className="p-2 border border-borderColor rounded-lg w-xs h-10" type="text" placeholder="Enter your username"></input>
+        </div>
+        <div className="mt-5 mb-5">
+          <h3 className="text-xl font-medium">Password</h3>
+          <input className="p-2 border border-borderColor rounded-lg w-xs h-10" type="password" placeholder="Enter your password"></input>
+        </div>
+        <div className="text-center m-3">
+          <button className="bg-bgButton text-textButton rounded-2xl p-3 font-medium w-full hover:bg-hoverbutton active:bg-hoverbutton">Sign In</button>
+        </div>
+      </div>
+    </div>
+  );
+}
