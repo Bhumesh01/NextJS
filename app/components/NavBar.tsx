625df9c899a451f40f60328ea387499125627bcd
Bhumesh Mahajan <167601736+Bhumesh01@users.noreply.github.com>
2026-01-05 19:54:47 +0530

    Routing using Link tag

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,10 +1,15 @@
+import Link from "next/link";
 export function Navbar(){
     return(
         <div className="p-5 flex justify-between border-b xl:pl-15 xl:pr-15  mb-5 border-b-borderColor">
             <h1 className="animate-pulse font-extrabold text-3xl "> BLOGS</h1>
             <div className="flex gap-5">
-                <Button name="Add Blogs"></Button>
-                <Button  name="Login"></Button>
+                <Link href="/signup">
+                    <Button name="Sign Up"></Button>
+                </Link>
+                <Link href="/signin">
+                    <Button name="Sign In"></Button>
+                </Link>
             </div>
         </div>
     )
