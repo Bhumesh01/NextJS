398ded509a4526a527f740ec72e27f04514a7d19
Bhumesh Mahajan <167601736+Bhumesh01@users.noreply.github.com>
2026-01-05 18:34:15 +0530

    Fetching data in Next.JS

---
+++ app/usercard/page.tsx
@@ -0,0 +1,43 @@
+import axios from "axios";
+
+async function getDetails(){
+    try{
+        const response = await axios.get("https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details");
+        return response.data;
+    }
+    catch(err){
+        console.log("Error fetching details");
+        return "";
+    }
+}
+
+interface responseType{
+  name: string,
+  email: string,
+  address: {
+    city: string,
+    state: string,
+    houseNumber: string
+  }
+}
+
+
+export default async function User(){
+    const data:responseType = await getDetails();
+    if(!data){
+        return(
+            <div className="flex justify-center items-center gap-10 align-middle h-96">
+                <div className="animate-pulse text-2xl flex justify-center items-center text-gray-800 font-semibold">Loading...</div>
+            </div>
+        )
+    }
+    return(
+        <div className="flex justify-center items-center gap-2 flex-col ">
+            <div className="border border-borderColor p-5 rounded-2xl shadow text-2xl">
+                <h3><b>Name:</b> {data.name}</h3>
+                <h3><b>Email:</b> {data.email}</h3>
+                <h3><b>Address:</b> {data.address.houseNumber}, {data.address.city},{data.address.state}</h3>
+            </div>
+        </div>
+    )
+}
