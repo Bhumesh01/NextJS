5a43fd3326a01a8be40835cf0df9a74d70a638e7
Bhumesh Mahajan <167601736+Bhumesh01@users.noreply.github.com>
2026-01-08 21:45:41 +0530

    LocalStorage error as its a server component

--- app/usercard/page.tsx
+++ app/usercard/page.tsx
@@ -1,53 +1,51 @@
-"use client"
 import axios from "axios";
-// import client from "../lib/db";
-import { useEffect, useState } from "react";
+import client from "../lib/db";
 // You don't need a separate endpoint for fetching user data, you can directly achieve this in this component itself, All the async logic remains in the server it is not sent to the client, only the HTML is sent to the client.
-// async function getDetails():Promise<ResponseType | null>{
-//     try{
-//         const response = await client.user.findFirst({});
-//         if (!response) return null;
-//         return {
-//             name: response.username,
-//             email: response.email,
-//             address: {
-//               city: response.city,
-//               state: response.state,
-//               houseNumber: response.houseNumber,
-//             },
-//         };
-//     }
-//     catch(err){
-//         console.log("Error fetching details");
-//         return null;
-//     }
-// }
+async function getDetails():Promise<ResponseType | null>{
+    try{
+        const response = await client.user.findFirst({});
+        if (!response) return null;
+        return {
+            name: response.username,
+            email: response.email,
+            address: {
+              city: response.city,
+              state: response.state,
+              houseNumber: response.houseNumber,
+            },
+        };
+    }
+    catch(err){
+        console.log("Error fetching details");
+        return null;
+    }
+}
 
-// async function getDetails2():Promise<ResponseType | null>{
-//     try{
-//         const token = localStorage.getItem("token");
-//         const response = await axios.get("http:localhost:3000/api/v1/user/details", {
-//             headers:{
-//                 Authorization: `Bearer ${token}`
-//             }
-//         })
-//         console.log(response, "Err")
-//         if (!response) return null;
-//         return {
-//             name: response.data.username,
-//             email: response.data.email,
-//             address: {
-//               city: response.data.city,
-//               state: response.data.state,
-//               houseNumber: response.data.houseNumber,
-//             },
-//         };
-//     }
-//     catch(err){
-//         console.log("Error fetching details");
-//         return null;
-//     }
-// }
+async function getDetails2():Promise<ResponseType | null>{
+    try{
+        const token = localStorage.getItem("token");
+        const response = await axios.get("http:localhost:3000/api/v1/user/details", {
+            headers:{
+                Authorization: `Bearer ${token}`
+            }
+        })
+        console.log(response, "Err")
+        if (!response) return null;
+        return {
+            name: response.data.username,
+            email: response.data.email,
+            address: {
+              city: response.data.city,
+              state: response.data.state,
+              houseNumber: response.data.houseNumber,
+            },
+        };
+    }
+    catch(err){
+        console.log("Error fetching details");
+        return null;
+    }
+}
 
 interface ResponseType{
   name: string,
@@ -60,24 +58,14 @@ interface ResponseType{
 };
 
 
-export default function User(){
+export default async function User(){
     // await new Promise(r=> setTimeout(r, 5000));
-    const [data, setData] = useState<ResponseType|null>(null)
-    useEffect(()=>{
-        const token = localStorage.getItem("token");
-        if (!token) return;
-        axios.get("http://localhost:3000/api/v1/user/details", {
-            headers:{
-                Authorization: `Bearer ${token}`
-            }
-        }).then((res)=>setData(res.data))
-        .catch(() => console.log("Error fetching details"));
-    }, [])
+    const data = await getDetails2();
     if(!data){
         return(
             <div className="flex justify-center items-center gap-2 flex-col ">
             <div className="border border-borderColor p-5 rounded-2xl shadow text-2xl">
-                <h3>Error Fetching USer</h3>
+                <h3>Error Fetching User</h3>
             </div>
             </div>
         )
