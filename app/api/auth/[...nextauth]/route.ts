09326d47adc1435e053339c057882dd980f28417
Bhumesh Mahajan <167601736+Bhumesh01@users.noreply.github.com>
2026-01-09 19:25:57 +0530

    Google/Github Providers

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,17 +1,21 @@
 import NextAuth from "next-auth"
 import  CredentialsProvider  from "next-auth/providers/credentials"
+import GoogleProvider from "next-auth/providers/google";
+import GithubProvider from "next-auth/providers/github";
 import client from "@/app/lib/db";
+import dotenv from "dotenv"
+dotenv.config();
 const handler = NextAuth({
     providers: [
         CredentialsProvider({
-            name: "Login with Email & Username",
+            name: "Email & Username",
             credentials:{
                 username: {label: "Username", type:"text", placeholder: "Enter Your username"},
                 email: {label: "Email", type:"text", placeholder: "Enter Your Email"},
-                password: {label: "Password", type: "password"}
+                password: {label: "Password", type: "password", placeholder: "Enter your Password"}
             },
             async authorize(credentials:any){
-                if (!credentials?.email || !credentials?.password) {
+                if (!credentials?.email || !credentials?.username || !credentials?.password) {
                      return null;
                 }
                 const user = await client.user.findFirst({
@@ -30,9 +34,17 @@ const handler = NextAuth({
                 }
                 return null;
             }
+        }),
+        GoogleProvider({
+            clientId: "fsv",
+            clientSecret: "klk"
+        }),
+        GithubProvider({
+            clientId: "",
+            clientSecret: ""
         })
     ],
-    secret: "hi"
+    secret: process.env.NEXTAUTH_SECRET
 })
 
 export { handler as GET, handler as POST }
