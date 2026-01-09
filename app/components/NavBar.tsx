e29465e526df12fe1937a40589286f633a628b8c
Bhumesh Mahajan <167601736+Bhumesh01@users.noreply.github.com>
2026-01-09 19:55:26 +0530

    sessions on the client

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,5 +1,17 @@
+"use client"
+import { SessionProvider, signIn, useSession } from "next-auth/react";
 import Link from "next/link";
+import {signOut} from "next-auth/react"
 export function Navbar(){
+    return<div>
+        <SessionProvider>
+            <NavbarComponent></NavbarComponent>
+        </SessionProvider>
+    </div>
+}
+
+function NavbarComponent(){
+    const session = useSession();
     return(
         <div className="p-5 flex justify-between border-b xl:pl-15 xl:pr-15  mb-5 border-b-borderColor">
             <Link href="/">
@@ -12,9 +24,11 @@ export function Navbar(){
                 <Link href="/signup">
                     <Button name="Sign Up"></Button>
                 </Link>
-                <Link href="/signin">
-                    <Button name="Sign In"></Button>
-                </Link>
+                <button className="bg-bgButton text-textButton rounded-2xl p-3 pl-10 pr-10 font-medium w-fit hover:bg-hoverbutton active:bg-hoverbutton" onClick={()=>{
+                    if(session.status==="authenticated")signOut();
+                    else signIn();
+                }} >{`${session.status==='authenticated'?'LogOut':'SignIn'}`}</button>
+                
             </div>
         </div>
     )
